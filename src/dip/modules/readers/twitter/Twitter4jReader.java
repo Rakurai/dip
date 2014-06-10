34c243f36655d44207564bb5bab433b16cc8f31f
groppcw <groppcw@GROPPCW-1.rose-hulman.edu>
2014-06-10 13:23:18 -0400

    Forks HBC and twitter4j readers into separate modules. Changes Twitter2Mongo to use twitter4j module.

---
+++ src/dip/modules/readers/twitter/Twitter4jReader.java
@@ -0,0 +1,75 @@
+package dip.modules.readers.twitter;
+
+import java.util.concurrent.BlockingQueue;
+import dip.modules.readers.AbstractReader;
+import twitter4j.StallWarning;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.TwitterStreamFactory;
+//import twitter4j.*;
+import twitter4j.TwitterStream;
+//import twitter4j.auth.AccessToken;
+//import twitter4j.conf.ConfigurationBuilder;
+import twitter4j.TwitterObjectFactory;
+
+public class Twitter4jReader extends AbstractReader<String> {
+	
+	public Twitter4jReader(BlockingQueue<String> q) {
+		super(q);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+    public void run() {
+        TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+        StatusListener listener = new StatusListener() {
+            @Override
+            public void onStatus(Status status) {
+                System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
+                //System.out.println(status.getUser().getName() + " : " + status.getText());
+                String rawjson = TwitterObjectFactory.getRawJSON(status);
+                try {
+                        q.put(rawjson);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            }
+
+            @Override
+            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+                System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
+            }
+
+            @Override
+            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+                System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
+            }
+
+            @Override
+            public void onScrubGeo(long userId, long upToStatusId) {
+                System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+            }
+
+            @Override
+            public void onStallWarning(StallWarning warning) {
+                System.out.println("Got stall warning:" + warning);
+            }
+
+            @Override
+            public void onException(Exception ex) {
+                ex.printStackTrace();
+            }
+        };
+        twitterStream.addListener(listener);
+        twitterStream.sample();
+    }
+
+	@Override
+	protected String read() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/dip/modules/readers/twitter/TwitterReader.java
+++ src/dip/modules/readers/twitter/TwitterReader.java
@@ -20,9 +20,6 @@ import twitter4j.StallWarning;
 import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
 import twitter4j.StatusListener;
-import twitter4j.TwitterStreamFactory;
-//import twitter4j.*;
-import twitter4j.TwitterStream;
 //import twitter4j.auth.AccessToken;
 //import twitter4j.conf.ConfigurationBuilder;
 import twitter4j.TwitterObjectFactory;
@@ -128,51 +125,6 @@ public class TwitterReader extends AbstractReader<String> {
         }
 	}
 	
-    public void deprecated_run() {
-        TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
-        StatusListener listener = new StatusListener() {
-            @Override
-            public void onStatus(Status status) {
-                System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
-                //System.out.println(status.getUser().getName() + " : " + status.getText());
-                String rawjson = TwitterObjectFactory.getRawJSON(status);
-                try {
-                        q.put(rawjson);
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-            }
-
-            @Override
-            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-                System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
-            }
-
-            @Override
-            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-                System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
-            }
-
-            @Override
-            public void onScrubGeo(long userId, long upToStatusId) {
-                System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
-            }
-
-            @Override
-            public void onStallWarning(StallWarning warning) {
-                System.out.println("Got stall warning:" + warning);
-            }
-
-            @Override
-            public void onException(Exception ex) {
-                ex.printStackTrace();
-            }
-        };
-        twitterStream.addListener(listener);
-        twitterStream.sample();
-    }
-
 	@Override
 	protected String read() {
 		// TODO Auto-generated method stub

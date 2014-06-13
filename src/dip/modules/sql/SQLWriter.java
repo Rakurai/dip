73fdb4ce6459f17ee4ae96f2997cc5a3e7d03444
Jason Anderson <jwa2@clemson.edu>
2014-06-13 16:47:47 -0400

    working SQL writing now

--- src/dip/modules/sql/SQLWriter.java
+++ src/dip/modules/sql/SQLWriter.java
@@ -1,8 +1,9 @@
 package dip.modules.sql;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.BlockingQueue;
@@ -11,29 +12,41 @@ import dip.modules.AbstractWriter;
 
 public class SQLWriter extends AbstractWriter<Map<String, Object>> {
 
-	Statement statement;
 	private String table_name;
+	private Connection connection;
 
 	public SQLWriter(BlockingQueue<Map<String, Object>> queue, Connection connection, String table_name) throws SQLException {
 		super(queue);
-		this.statement = connection.createStatement();
+		this.connection = connection;
 		this.table_name = table_name;
 	}
 
 	@Override
-	protected void write(Map<String, Object> pairs) throws Exception {
-		String sql = "INSERT INTO " + table_name + " VALUES(" + generateValues(pairs) + ")";
-		statement.executeUpdate(sql);
-	}
-
-	private String generateValues(Map<String, Object> map) {
-		StringBuilder sb = new StringBuilder();
+	protected void write(Map<String, Object> map) throws Exception {
+		ArrayList<Object> vals = new ArrayList<Object>();
+		StringBuilder keyString = new StringBuilder();
+		StringBuilder valString = new StringBuilder();
+		boolean first = true;
+		
 		for (Entry<String, Object> entry: map.entrySet()) {
-			sb.append(entry.getKey());
-			sb.append('=');
-			sb.append(entry.getValue());
-			sb.append(',');
+			if (!first) {
+				keyString.append(',');
+				valString.append(',');
+			}
+			else
+				first = false;
+			keyString.append(entry.getKey());
+			valString.append('?');
+			vals.add(entry.getValue());
 		}
-		return sb.toString();
+
+		// INSERT INTO table (key1,key2,...) VALUES (val1,val2,...)
+		String sql = "INSERT INTO " + table_name + " (" + keyString.toString() + ") VALUES (" + valString.toString() + ")";
+		PreparedStatement pst = connection.prepareStatement(sql);
+
+		for (int i = 0; i < vals.size(); i++)
+			pst.setObject(i+1, vals.get(i));
+
+		pst.executeUpdate();
 	}
 }
